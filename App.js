e0071bca6251646cb8e9aa0e843211d8b544dbb6
Nishant Kohli <nishkohli96@gmail.com>
2021-08-31 18:35:06 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => {
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <View>
+                <Text>hi</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default App;
