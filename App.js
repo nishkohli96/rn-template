ee00c08bd0edcb06f3f8c167fbe06fb2ab16b6f7
Nishant Kohli <nishkohli96@gmail.com>
2021-08-31 18:55:29 +0530

    added async-storage

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storeData = async value => {
+    try {
+        await AsyncStorage.setItem('@storage_Key', value);
+    } catch (e) {
+      return;
+    }
+};
+
+const getData = async () => {
+    try {
+        const value = await AsyncStorage.getItem('@storage_Key');
+        if (value !== null) {
+            console.log(value);
+            return value;
+        }
+    } catch (e) {
+        return 'error';
+    }
+};
 
 const App = () => {
+    storeData('nish');
+    getData();
     return (
-        <SafeAreaView style={backgroundStyle}>
+        <SafeAreaView>
             <View>
                 <Text>hi</Text>
             </View>
---
+++ README.md
@@ -0,0 +1,5 @@
+# RnTemplate
+
+## Developer Guide
+
+- Install [Async Storage](https://react-native-async-storage.github.io/async-storage/). Needs to be manually linked. Go to [this thread](https://stackoverflow.com/questions/61479644/invalid-podfile-file-unable-to-locate-the-executable-node) if `npx pod-install` fails to run. 
