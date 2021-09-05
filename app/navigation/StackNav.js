3fd7cf39af759964c9920b85b6bac57d4df27363
nish <nishant.kohli@storydigital.in>
2021-09-06 02:14:58 +0530

    android back handler sorta done

--- app/navigation/StackNav.js
+++ app/navigation/StackNav.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Platform, BackHandler, Alert } from 'react-native';
 import { observer } from 'mobx-react-lite';
-import { useFocusEffect, NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import DrawerNav from './DrawerNav';
@@ -11,38 +10,6 @@ import { useOnboardStore } from '_Store/onboard.store';
 const StackNav = () => {
     const Stack = createStackNavigator();
     const { isOnboard } = useOnboardStore();
-
-    // if (Platform.OS === 'android') {
-    //     useFocusEffect(
-    //         React.useCallback(() => {
-    //             const backAction = () => {
-    //                 Alert.alert(
-    //                     'Hold on!',
-    //                     'Are you sure you want to go back?',
-    //                     [
-    //                         {
-    //                             text: 'Cancel',
-    //                             onPress: () => null,
-    //                             style: 'cancel',
-    //                         },
-    //                         {
-    //                             text: 'YES',
-    //                             onPress: () => BackHandler.exitApp(),
-    //                         },
-    //                     ],
-    //                 );
-    //                 return true;
-    //             };
-
-    //             const backHandler = BackHandler.addEventListener(
-    //                 'hardwareBackPress',
-    //                 backAction,
-    //             );
-
-    //             return () => backHandler.remove();
-    //         }, []),
-    //     );
-    // }
 
     return (
         <NavigationContainer>
